161cfc84d89132c9a3e2c47ae674c229cc801d4e
AedrianF <aedrianformento@gmail.com>
2024-03-28 23:29:58 -0700

    Added more basic files and server running to open a basic index html page, must install express package for it to work

--- server/server.js
+++ server/server.js
@@ -1,1 +1,39 @@
-// Code he will be for the server
+(() => {
+    //const config = require(`${__dirname}/config/config`)
+    //const Utils = require(`${__dirname}/utils`)
+    const fs = require('fs')
+    const express = require('express')
+    const app = express()
+    const logs = []
+    const config = require(`${__dirname}/config/config`)
+
+    /**
+     * Middleware declarations
+     */
+    app.use(express.json());
+    app.use((request, response, next) => {
+        config.logFile(request, logs)
+        next()
+    })
+    
+    app.use(express.static(`${__dirname}/../views`))
+    app.get('/',  (request, response) => {
+        config.logFile(request, logs)
+        response.render(`${config.ROOT}/index`, {page: "index", title: "Home page"})
+    })
+    app.get('/index', (request, response) => {
+        response.send(`${config.ROOT}/index`)
+    })
+    
+
+    // Start Node.js HTTP webserver
+    app.listen(config.PORT, "127.0.0.1", () => {
+        console.log(`\t|Server listening on ${config.PORT}`)
+    })
+})()
+
+
+
+
+
+
