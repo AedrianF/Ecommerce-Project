31985c9d95ab2eb53b0a347cffa8620f1ead8693
ParasK1 <paras082k@gmail.com>
2024-04-12 16:09:27 -0700

    Paras's code

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -6,6 +6,12 @@ const logs = []
 
 homeController.get('/index', util.logRequest , async (request, response) => {
     console.log("Inside get /index")
+
+    //1. connect to DB
+    //2. get all products
+    //3. loop thru products and display them
+
+
     response.sendFile('/index.html', {'root': config.ROOT})
 })
 
